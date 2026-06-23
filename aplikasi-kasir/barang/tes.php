d8482f9ba9915f4f1b7498489a42aa29e612b68c
ilhamramadhanm18-hue <ilhamramadhanm18@gmail.com>
2026-06-24 03:18:18 +0700

    Delete aplikasi-kasir directory

--- aplikasi-kasir/barang/tes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "TES BERHASIL";
-
-?>
--- aplikasi-kasir/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost","root","","sibsikasir");
-
-if(!$conn){
-    die("Koneksi gagal : ".mysqli_connect_error());
-}
-
-?>
